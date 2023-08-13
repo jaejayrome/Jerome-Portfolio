4eca9a499ec85ef93a76038c49ddb9a71818187d
Jerome Goh <jerome.goh.jg@gmail.com>
2023-08-13 10:58:58 +0800

    check word wrapping bug fixed on deployed site

--- jerome-portfolio/src/app/about.tsx
+++ jerome-portfolio/src/app/about.tsx
@@ -1,95 +1,53 @@
 import {motion} from 'framer-motion';
 
 export default function About() {
-    
-    const text : string = "Year 2 Information Systems student with a second major in Quantitative Finance studying at the National University of Singapore.";
-    const text2: string = "I enjoy coding as a hobby because nothing in the world is more satisfying than solving bugs. "
-    const text3: string = "My first experience with code was self learning Java after my national service."
-    const text4: string = "Asides from that, I currently learn Korean as a third language and plan to take my exchange studies there!";
-    const letters : string[] = Array.from(text)
-    const letters2: string[] = Array.from(text2)
-    const letters3: string[] = Array.from(text3)
-    const letters4: string[] = Array.from(text4)
+    const texts = [
+        "Year 2 Information Systems student with a second major in Quantitative Finance studying at the National University of Singapore.",
+        "I enjoy coding as a hobby because nothing in the world is more satisfying than solving bugs. ",
+        "My first experience with code was self learning Java after my national service.",
+        "Asides from that, I currently learn Korean as a third language and plan to take my exchange studies there!"
+    ];
 
     const container = {
-        hidden: {opacity: 0},
-        visible: (i = 1) => ({
+        hidden: { opacity: 0 },
+        visible: { 
             opacity: 1, 
-            transition: {staggerChildren: 0.01, delayChildren: 0.04 * i},
-        }),
+            transition: { staggerChildren: 0.1 }
+        },
     };
 
     const child = {
+        hidden: { opacity: 0, x: -20 },
         visible: {
             opacity: 1, 
-            x: 0, 
-            y: 0,
+            x: 0,
             transition: {
                 type: "spring",
                 damping: 12, 
                 stiffness: 100,
-            },
-        },
-        hidden: {
-            opacity: 0, 
-            x: -20,
-            y: -20, 
-            transition: {
-                type: "spring",
-                damping: 12, 
-                stiffness: 100,
-            },
+            }
         }
     };
-
 
     return (
         <div>
             <div className="dark:text-white text-slate-600 font-topLeft text-4xl"> 
                 About Me
             </div>
-
-            <motion.div className="flex flex-wrap max-w-1/2 text-slate-700 dark:text-slate-400 px-6 overflow-hidden text-lg mobile:text-2xl leading-" variants={container} initial= "hidden" animate= "visible">
-
-                <div className='flex flex-wrap'>
-                {letters.map((word, index) => (
-                    <motion.p variants={child} key={index}>
-                        {word === " " ? "\u00A0" : word}
-                    </motion.p>
+            <motion.div className="flex flex-col max-w-1/2 text-slate-700 dark:text-slate-400 overflow-hidden text-md mobile:text-2xl"
+                        variants={container}
+                        initial="hidden"
+                        animate="visible">
+                {texts.map((text, index) => (
+                    <div className='break-words' key={index}>
+                        {text.split(' ').map((word, wordIndex) => (
+                            <motion.span className='inline-block' variants={child} key={wordIndex}>
+                                {word + "\u00A0"}
+                            </motion.span>
+                        ))}
+                    </div>
                 ))}
-                </div>
-
-          
-                <div className='flex flex-wrap'>
-                {letters2.map((word, index) => (
-                <motion.p variants={child} key={index}>
-                    {word === " " ? "\u00A0" : word}
-                </motion.p>
-                ))} 
-                </div>
-
-
-                <div className='flex flex-wrap'>
-                {letters3.map((word, index) => (
-                <motion.p variants={child} key={index}>
-                    {word === " " ? "\u00A0" : word}
-                </motion.p>
-                ))} 
-                </div>
-
-
-                <div className='flex flex-wrap'>
-                {letters4.map((word, index) => (
-                <motion.p variants={child} key={index}>
-                    {word === " " ? "\u00A0" : word}
-                </motion.p>
-                ))} 
-                </div>
-
-                
-                
             </motion.div>
-            
         </div>
     )
 }
--- jerome-portfolio/src/app/experience.tsx
+++ jerome-portfolio/src/app/experience.tsx
@@ -1,16 +1,32 @@
+import Image from "next/image";
+import VerticalImage from '../images/vertical_timeline.png'
+import NUSLightLogo from '../images/nus_light_logo.png'
+
 export default function Experience() {
    return (
-        <div className="min-w-full">
-            <div className="text-slate-600 dark:text-white font-topLeft text-4xl mobile:text-4xl"> 
+        <div className="min-w-full overflow-hidden">
+            {/* <div className="text-slate-600 dark:text-white font-topLeft text-4xl mobile:text-4xl"> 
                 Relevant Experiences
             </div>
 
-            <div className="flex flex-col justify-start min-w-full text-slate-600 dark:text-white text-2xl mobile:text-4xl">
+            <div className="flex flex-col justify-center items-center min-w-full text-slate-600 dark:text-white text-2xl mobile:text-4xl">
 
-                {/* Experience Container */}
+                Experience Container
+                <div className="flex flex-row justify-between items-center min-w-full space-x-4">
+                    <Image src= {NUSLightLogo} className = "w-1/3 h-full" alt = ""></Image>
+                    <Image src = {VerticalImage} className = "h-full" alt = ""></Image>
+                    <ul className=" text-sm overflow-hidden ">
+                    <li className="flex flex-wrap">
+                            Managed a class of 30 students 
+                        </li>
+                        <li className="flex flex-wrap">
+                            Taught Object-Oriented Programming and Functional Programming in Java
+                        </li>
+                    </ul>
+                </div>
                 
-
-            </div>
+                
+            </div> */}
 
 
 
